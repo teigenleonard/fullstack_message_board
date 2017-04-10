61cbdbc02cb564cb9cfc98fcf3f880882e1950d2
Teigen Leonard <teigenl@gmail.com>
2017-04-10 15:53:50 -0500

    Challenge not quite met

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,5 +1,24 @@
 var myApp = angular.module('myApp', []);
 
+myApp.controller('InputController', ['$scope', '$http', 'DataService',function( $scope, $http, DataService){
+  console.log('InputController');
+
+  $scope.info = {
+    name : '',
+    message : ''
+  };
+
+  $scope.postData = DataService.postData;
+
+}]);
+
+myApp.controller('DisplayController', ['$scope', '$http', 'DataService',function( $scope, $http, DataService){
+ console.log('DisplayController');
+
+ $scope.messages = DataService.getData;
+
+}]);
+
 myApp.factory('DataService', ['$http', function($http){
     var messageObject = {
       messages : []
@@ -7,13 +26,13 @@ myApp.factory('DataService', ['$http', function($http){
 
     function getData(){
       $http.get('/messages').then(function(response){
-
+        console.log('getData response:' response);
       });
     }
 
     function postData(message){
       $http.post('/messages', message).then(function(response){
-
+        // console.log(response);
       });
     }
 
